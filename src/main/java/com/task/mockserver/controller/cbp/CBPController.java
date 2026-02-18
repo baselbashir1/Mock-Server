f0c2a98f30312565e89fdef8ccbecb542cf2eedb
Basel Bashir <eng.baselbashir@gmail.com>
2026-02-18 15:19:40 +0300

    fix

--- src/main/java/com/task/mockserver/controller/cbp/CBPController.java
+++ src/main/java/com/task/mockserver/controller/cbp/CBPController.java
@@ -4,6 +4,7 @@ import com.task.mockserver.service.cbp.CBPService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,20 +14,56 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/tmf-api/productManagement/v4")
 public class CBPController {
 
-    @GetMapping("/product")
+    @GetMapping(value = "/product", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> getSubscriptionDetails(
             @RequestParam(value = "realizingResource.id", required = false) String realizingResourceId,
             @RequestParam(value = "productOffering.id", required = false) String productOfferingId,
             @RequestHeader(value = "correlationId", required = false) String correlationId) {
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(CBPService.buildResponse());
-    }
+        String successResponse = """
+                [
+                    {
+                        "id": "0094579689186968162693432801636549265864",
+                        "status": "active",
+                        "startDate": "2024-08-14T06:54:58",
+                        "terminationDate": "2024-12-14T06:54:58",
+                        "productOffering": {
+                            "id": " USEmb_Post_Hybrid_USG_500 "
+                        },
+                        "productPrice": [
+                            {
+                                "priceType": "ONETIME",
+                                "price": {
+                                    "dutyFreeAmount": {
+                                        "unit": "K",
+                                        "value": "451.0"
+                                    }
+                                }
+                            }
+                        ],
+                        "productCharacteristic": [
+                            {
+                                "name": "paymentMode",
+                                "value": "MoMO"
+                            }
+                        ],
+                        "realizingResource": [
+                            {
+                                "id": "9371987370",
+                                "type": "resourceNumber"
+                            }
+                        ]
+                    }
+                ]
+                """;
 
-    @GetMapping("/failure")
-    public ResponseEntity<?> getFailure() {
-        return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(CBPService.buildFailure());
+        String failureResponse = """
+                {
+                    "code": "2002",
+                    "message": "Application Internal Error",
+                    "status": "Fail"
+                }
+                """;
+
+        return ResponseEntity.status(HttpStatus.OK).body(CBPService.buildResponse());
     }
 }
