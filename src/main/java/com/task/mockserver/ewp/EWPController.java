b15840491c22f2bb7915c07a9e3d4a9d62ee3c6d
Basel Bashir <eng.baselbashir@gmail.com>
2026-05-11 15:49:06 +0300

    Update ewp

--- src/main/java/com/task/mockserver/ewp/EWPController.java
+++ src/main/java/com/task/mockserver/ewp/EWPController.java
@@ -1,31 +1,27 @@
 package com.task.mockserver.ewp;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/payments")
 public class EWPController {
 
-    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-    public ResponseEntity<?> payments() {
+    @PostMapping(value = "/madapi/payment", produces = MediaType.APPLICATION_XML_VALUE)
+    public ResponseEntity<?> payment(@RequestBody String body) {
+        System.out.println(body);
+
         String response = """
-                <ns0:paymentrequest xmlns:ns0="http://www.ericsson.com/em/emm/serviceprovider/v1_1/frontend">
-                    <referenceid>referenceid</referenceid>
-                    <sendingfri>FRI:260765647419/MSISDN</sendingfri>
-                    <receivingfri>FRI:260967738080@airtime.OCS/SP</receivingfri>
-                    <amount>
-                        <amount>15.00</amount>
+                <?xml version="1.0" encoding="UTF-8"?>
+                <ns10:paymentresponse xmlns:ns10="http://www.ericsson.com/em/emm/serviceprovider/v1_1/frontend" xmlns:fic="http://www.ericsson.com/em/emm/financial/v1_2/common" xmlns:ns11="http://www.ericsson.com/em/emm/financial/v1_2" xmlns:ns12="http://www.ericsson.com/em/emm/financial/v1_0" xmlns:ns13="http://www.ericsson.com/em/emm/financial/v1_1" xmlns:ns4="http://www.ericsson.com/em/emm/financial/v1_0/common" xmlns:ns5="http://www.ericsson.com/em/emm/v2_1/common" xmlns:ns6="http://www.ericsson.com/em/emm/financial/v1_1/common" xmlns:ns8="http://www.ericsson.com/em/emm/v1_1/common" xmlns:op="http://www.ericsson.com/em/emm/v1_0/common" xmlns:spc="http://www.ericsson.com/em/emm/serviceprovider/v1_0/common" xmlns:xs="http://www.w3.org/2001/XMLSchema">
+                    <fee>
+                        <amount>0.00</amount>
                         <currency>ZMW</currency>
-                    </amount>
-                    <sendernote>sendernote</sendernote>
-                    <receivermessage>receivermessage</receivermessage>
-                    <includesendercharges>false</includesendercharges>
-                </ns0:paymentrequest>
+                    </fee>
+                    <transactionid>538195</transactionid>
+                </ns10:paymentresponse>
                 """;
-        return ResponseEntity.ok().body(response);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
