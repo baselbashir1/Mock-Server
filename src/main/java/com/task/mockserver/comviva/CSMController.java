09551f5717f1007e43592e5aa00caa01f292454d
Basel Bashir <eng.baselbashir@gmail.com>
2026-09-11 15:16:17 +0300

    Add comviva

---
+++ src/main/java/com/task/mockserver/comviva/CSMController.java
@@ -0,0 +1,38 @@
+package com.task.mockserver.comviva;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CSMController {
+
+    // http://172.25.10.111:8087/AFL/activationReq
+    @PostMapping(value = "/AFL/activationReq", produces = MediaType.APPLICATION_XML_VALUE)
+    public ResponseEntity<?> payment(@RequestBody Object body) {
+        String response = """
+                <ocsResponse>
+                    <serviceType></serviceType>
+                    <resultCode>3099</resultCode>
+                    <result>Invalid plan ID</result>
+                    <sequenceNo>test14522</sequenceNo>
+                    <callingParty>2609132059261</callingParty>
+                    <cpcgFlag>10</cpcgFlag>
+                    <calledParty>-1</calledParty>
+                    <bearerId>wwwwff</bearerId>
+                    <startTime>260908081742</startTime>
+                    <planId>12345</planId>
+                    <serviceNode></serviceNode>
+                    <serviceId></serviceId>
+                    <requestType>20</requestType>
+                    <inTranxId></inTranxId>
+                    <inResultCode></inResultCode>
+                </ocsResponse>
+                """;
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
